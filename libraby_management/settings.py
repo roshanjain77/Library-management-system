649f6fef204adcfa6f72f83ce7e1057494972d6e
sacoder8031 <sharabh.agrawal.cse19@itbhu.ac.in>
2020-11-24 00:23:40 +0530

    set developers and mentors cards

--- libraby_management/settings.py
+++ libraby_management/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'my_db',
         'USER': 'root',
-        'PASSWORD': 'RoseRoshan773289#',
+        'PASSWORD': 'SDL1706VB',
         'HOST': 'localhost',
         'OPTIONS': {
             'init_command': 'SET default_storage_engine=INNODB'
